135b0f422f860fc454a6b9fa348b69266aa92c5a
marianj <marian.juanir@novaliches.sti.edu>
2014-01-21 16:27:41 +0800

    test commit

---
+++ hiWorld/src/sayHi.java
@@ -0,0 +1,17 @@
+
+public class sayHi {
+
+	public sayHi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hi, world!");
+	}
+
+}
